e696d7e2e6ce1afa5a6cc2b11aa2d77545f1743d
sasha_zuev <sashazuev01@gmail.com>
2020-12-28 08:56:27 +0200

    Template

--- handlers/users/getting_picture.py
+++ handlers/users/getting_picture.py
@@ -1,14 +1,12 @@
-from aiogram.types import ContentType, Message
+from aiogram.types import ContentType, Message, ParseMode, InputFile
 from loader import dp
 from utils.misc import rate_limit
 
 
-@rate_limit(limit=5)  #anti-spam
+@rate_limit(limit=5)  # anti-spam
 @dp.message_handler(content_types=ContentType.PHOTO)
 async def getting_photo(message: Message):
-    """Downloading to directory picture from message. Selecting background color from pic"""
-    try:
-        await message.photo[-1].download('pictures/picture.png')
-        await message.answer("Choose background color:")
-    except Exception as err:
-        await message.answer(f'Oops, some unknown error\n{err}')
+    """Downloading to directory picture from message."""
+    await message.photo[-1].download('pictures/picture.png')
+
+

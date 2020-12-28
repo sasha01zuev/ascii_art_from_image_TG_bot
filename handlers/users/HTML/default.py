ad95079cdb74ba11da29814351236ddf79e1d3f9
sasha_zuev <sashazuev01@gmail.com>
2020-12-28 15:55:19 +0200

    Default html done!

--- handlers/users/HTML/default.py
+++ handlers/users/HTML/default.py
@@ -1,29 +1,32 @@
+import os
+import shutil
+
 import ascii_magic
 import imgkit
-import os, shutil
 from aiogram.types import InputFile, CallbackQuery
-from loader import dp
+
 from keyboards.inline.callback_data import html_customization_callback
+from loader import dp
 
 
 @dp.callback_query_handler(html_customization_callback.filter(type='default'))
 async def default_html(call: CallbackQuery):
+    """Converting image to html, and taking screenshot of html page"""
     await call.answer(cache_time=3)
-    # await call.message.delete()
     username = call.from_user.username
 
     await call.message.edit_text('Wait some seconds...')
     output = ascii_magic.from_image_file(f'docs/{username}/picture.png',
-                                         columns=135, mode=ascii_magic.Modes.HTML)
-    ascii_magic.to_html_file(f'docs/{username}/ascii.html', output)
+                                         columns=135, mode=ascii_magic.Modes.HTML)  # Customization for default html doc
+    ascii_magic.to_html_file(f'docs/{username}/ascii.html', output)  # Reversing from image to html
 
-    imgkit.from_file(f'docs/{username}/ascii.html', f'docs/{username}/html.png')
-    size_b = os.path.getsize(f'docs/{username}/html.png')
-    size_mb = size_b / 1_000_000
+    imgkit.from_file(f'docs/{username}/ascii.html', f'docs/{username}/html.png')  # Screenshot from html page
+    size_b = os.path.getsize(f'docs/{username}/html.png')  # Size of screenshot in bytes
+    size_mb = size_b / 1_000_000  # Size of screenshot in mb
 
     await call.message.edit_text('Done! Sending ...')
 
-    if size_mb < 10:
+    if size_mb < 10:  # restriction of telegram. Image can't be more than 10mb
         await call.message.answer_photo(InputFile(path_or_bytesio=f'docs/{username}/html.png'))
         await call.message.answer_document(InputFile(f'docs/{username}/html.png'))
         await call.message.answer_document(InputFile(f'docs/{username}/ascii.html'))

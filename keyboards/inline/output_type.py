9a9464e65e2bc7faa86bd37328c5f6422855a179
sasha_zuev <sashazuev01@gmail.com>
2020-12-30 14:26:10 +0200

    Added invisible dir docs/

--- keyboards/inline/output_type.py
+++ keyboards/inline/output_type.py
@@ -5,7 +5,7 @@ from keyboards.inline.callback_data import output_callback
 output_type_keyboard = InlineKeyboardMarkup(row_width=1,
                                             inline_keyboard=[
                                                 [
-                                                    InlineKeyboardButton(text="HTML",
+                                                    InlineKeyboardButton(text="HTML (Colorized)",
                                                                          callback_data=output_callback.new(
                                                                              type="html")),
                                                     InlineKeyboardButton(text="ASCII",
